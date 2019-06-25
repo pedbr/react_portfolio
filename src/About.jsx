acb230dcaa16e41f9239415c3e5473ef8be75451
pedro_bras <pedro.c.bras@gmail.com>
2019-06-25 10:34:17 +0200

    adds ilustration to about and projects pages

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,23 @@
 import React from "react"
+import { UndrawHello } from 'react-undraw-illustrations';
 
 const About = () => {
     return (
-        <div>
-            <h1 className="content-text">About Me</h1>
-            <p className="content-text">Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-        </div>)
+        <div className="content-wrapper">
+            <div className="flex flex-wrap mb-4">
+                <div className="w-full lg:w-1/4 md:w-1/2">
+                    <UndrawHello primaryColor='#12283a' height='200px'/>
+                </div>
+                <div className="w-full lg:w-3/4 md:w-1/2">
+                    <h1 className="content-text">About Me</h1>
+                    <p className="content-text">Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+                </div>
+    
+            </div>
+        </div>
+    )
+    
 }
+
 
 export default About
