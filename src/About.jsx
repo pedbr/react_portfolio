5c86d3a654140d3a8c9ca87cfb2cde27a9ed0c0a
pedro_bras <pedro.c.bras@gmail.com>
2019-07-01 14:11:11 +0200

    adds link to email me on about page

--- src/About.jsx
+++ src/About.jsx
@@ -11,9 +11,9 @@ const About = () => {
                 <div className="w-full lg:w-3/4 md:w-1/2">
                     <h1 style={aboutTitle} className="content-text">about me .</h1>
                     <p style={aboutText} className="content-text">Hej! I'm Pedro, a 29 years old portuguese citizen based in Stockholm, Sweden. I have an academic background in Multimedia Design, and throghout my work life I had the pleasure of working in many different contexts. In my home country Portugal I worked as a Graphic Designer, Animator, Advertisement Content Creator and Web Designer. After deciding to expand my horizons I moved to Barcelona, Spain. Here I worked with customer service in the Travel Business, building my way up to a Corporate Trainer position. That position got me an oportunity to move and work in Stockholm, Sweden. After a year in the cold north I felt the need to get back to the creative fields. This time I decided to explore one of my biggest passions: coding. I applied for a 12 Week Full Stack Development Coding bootcamp, and am now starting my carrer as a Junior Software Developer! If you want to get in contact, just hit the button! </p>
-                    <button style={detailsBtn} className="bg-black text-yellow hover:bg-grey-darkest text-xs py-1 px-2">
+                    <a href="mailto:pedro.c.bras@gmail.com" target="_blank" style={detailsBtn} className="bg-black text-yellow hover:bg-grey-darkest text-xs py-1 px-2">
                     Contact .
-                </button>
+                </a>
                 </div>
                 
             </div>
@@ -26,7 +26,8 @@ const detailsBtn = {
     fontFamily: 'karla',
     letterSpacing: '-0.10em',
     marginLeft: '20px',
-    marginTop: '20px'
+    marginTop: '20px',
+    textDecoration: 'none',
 }
 
 const aboutTitle = {
