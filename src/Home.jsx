221e29d094645c9a4b40b506f4c6ee0441cc7e93
pedro_bras <pedro.c.bras@gmail.com>
2019-07-31 15:13:19 +0200

    moves home page to separate component

---
+++ src/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import Hello from "./Hello"
+import Header from "./Header"
+import Work from "./Work"
+import Projects from "./Projects"
+import About from "./About"
+import { Switch, Route } from 'react-router-dom'
+
+const Home = () => {
+    return (
+        <div className="page-wrapper">
+            <div className="page-content">
+                <Header />
+                <div className="content-wrapper">
+                    <Switch>
+                        <Route exact path='/' component={Hello}></Route>
+                        <Route exact path='/about' component={About}></Route>
+                        <Route exact path='/projects' component={Projects}></Route>
+                        <Route exact path='/work' component={Work}></Route>
+                    </Switch>
+                </div>
+                
+            </div >
+        </div >
+    )
+};
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Hello from "./Hello"
-import Header from "./Header"
-import Footer from "./Footer"
-import Work from "./Work"
 import './css/tailwind.css'
 import { BrowserRouter } from 'react-router-dom'
-import Projects from "./Projects"
-import About from "./About"
-import { Switch, Route } from 'react-router-dom'
+import Home from "./Home"
 
 const App = () => {
     return (
-        <div className="page-wrapper">
-            <div className="page-content">
-                <Header />
-                <div className="content-wrapper">
-                    <Switch>
-                        <Route exact path='/' component={Hello}></Route>
-                        <Route exact path='/about' component={About}></Route>
-                        <Route exact path='/projects' component={Projects}></Route>
-                        <Route exact path='/work' component={Work}></Route>
-                    </Switch>
-                </div>
-                <Footer />
-            </div >
-        </div >
+        <Home />
     )
 };
 
