7007b79dd1702b355536241265079a7a6eb91713
pedro_bras <pedro.c.bras@gmail.com>
2019-07-31 15:23:46 +0200

    sets up intro view and adds basic styling

--- src/index.js
+++ src/index.js
@@ -6,9 +6,32 @@ import Home from "./Home"
 
 const App = () => {
     return (
-        <Home />
+        <div className="page-wrapper">
+            <div>
+                <div className= "flex-wrap" style={centerDiv}>
+                    <p>Hi,</p>
+                    <p>I'm</p>
+                    <p>Pedro .</p>
+                </div>
+            </div>
+        </div>
+        // <Home />
     )
 };
+
+const centerDiv = {
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: 'karla',
+    fontWeight: 'bold',
+    fontSize: '144px',
+    lineHeight: '122px',
+    letterSpacing: '-0.10em',
+    color: '#000000',
+    
+}
 
 ReactDOM.render((
     <BrowserRouter>
