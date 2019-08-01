b5a8325530d170784dae787573626fd43a5ba9ed
pedro_bras <pedro.c.bras@gmail.com>
2019-08-01 22:14:59 +0200

    adds style structure to greeting page

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -6,28 +6,15 @@ const Hello = () => {
             <div className="center-div">
                 <div className= "greeting">
                     <div>Hey John!</div>
-                    <div>I'm Pedro.</div>
+                    <div>I'm Pedro .</div>
                 </div>
                 <div className="center-bar"></div>
-                <div style={bottomDiv}>
+                <div className="bottom-div">
                     <p>Junior Developer .</p>
                     <p>Creative Designer .</p>
                 </div>
             </div>
         )
-}
-
-const bottomDiv = {
-    marginLeft: '40.5%',
-    marginTop: '1%',
-    textAlign: 'left',
-    fontFamily: 'karla',
-    fontWeight: 'bold',
-    fontSize: '24px',
-    lineHeight: '114.9%',
-    letterSpacing: '-0.10em',
-    color: '#000000',
-    
 }
 
 export default Hello
