6998846c9320b41261962a949b2391b3898285b3
pedro_bras <pedro.c.bras@gmail.com>
2019-08-02 11:07:32 +0200

    removes inline styling from footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,19 +2,13 @@ import React from "react"
 
 const Footer = () => {
         return (
-            <div className="footer-wrapper">
-                <div style={footerStyle} className="footer-content">
+            <div className="">
+                <div className="">
                         Made with React .
                 </div>
             </div>
         )
 
-}
-
-const footerStyle ={
-    fontFamily: 'karla',
-    letterSpacing: '-0.10em',
-    fontWeight: 'bold',
 }
 
 export default Footer
