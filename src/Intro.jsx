42b76dccb7527bf6d4b4f9b47c61b168c22802c7
pedro_bras <pedro.c.bras@gmail.com>
2019-08-02 15:37:25 +0200

    adds conditions for user input, if no input, pop up alert with show

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import '../src/css/intro.css'
 
 const Intro = () => {
@@ -10,12 +10,21 @@ const Intro = () => {
     const inpValue = document.getElementById("inpValue");
     const submitBtn = document.getElementById("submit-button");
 
+    inpValue.addEventListener("keyup", function(event){
+        if (event.keyCode === 13){
+            event.preventDefault();
+            document.getElementById("submit-button").click();
+        }
+
+    })
+
     submitBtn.onclick = function () {
         const key = inpKey;
         const value = inpValue.value;
 
         if (value) {
-            localStorage.setItem(key, value)
+            localStorage.setItem(key, value);
+            window.open('/hello', "_self");
         } else {
             window.alert("Enter you name bitch!");
         }
@@ -31,8 +40,8 @@ const Intro = () => {
                     <div>Don't be a stranger !</div>
                     <div className='centerQuestion'>What's your name?</div>
                     <div className='name-submit'>
-                        <input type='text' className='inputBox' id="inpValue" autocomplete="off" placeholder=''></input>
-                        <div id="submit-button"><NavLink to='/hello'>GO!</NavLink></div>
+                        <input type='text' className='inputBox' id="inpValue" autoComplete="off" placeholder=''></input>
+                        <div id="submit-button">GO!</div>
                     </div>
                 </div>
             </div>
