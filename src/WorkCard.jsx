fc1b788e1e2207f42cd6e495ed63212600d9e25d
pedro_bras <pedro.c.bras@gmail.com>
2019-08-02 11:04:39 +0200

    remooves inline styling from work cards

--- src/WorkCard.jsx
+++ src/WorkCard.jsx
@@ -5,51 +5,20 @@ const WorkCard = (props) => {
     return (
 
         <div key={work.id}>
-            <img src={work.image} className="block h-auto w-full" style={{ height: '300px', objectFit: 'cover' }} />
+            <img src={work.image} className="" />
 
 
-            <div className="px-6 py-4" style={{ minHeight: '100px' }} >
-                <div style={workTitle} className="font-bold text-xl mb-2 text-yellow bg-black">{work.date} | {work.name} .</div>
-                <p style={companyStyle}>{work.company}</p>
-                <p style={locationStyle}>{work.location}</p>
-                <p style={descriptionStyle} className="text-black text-base">
+            <div className="" >
+                <div className="">{work.date} | {work.name} .</div>
+                <p>{work.company}</p>
+                <p>{work.location}</p>
+                <p className="">
                     {work.description}
                 </p>
-                <p style={divisorStyle}>_________________________________________</p>
+                <p>_________________________________________</p>
             </div>
         </div>
     )
-}
-
-const companyStyle = {
-    fontFamily: 'karla',
-    letterSpacing: '-0.10em',
-    fontWeight: 'bold',
-    fontSize: '24px',
-    marginBottom: '10px',
-}
-
-const locationStyle = {
-    fontFamily: 'karla',
-    letterSpacing: '-0.10em',
-    fontWeight: 'bold',
-    marginBottom: '5px',
-}
-
-const workTitle = {
-    fontFamily: 'karla',
-    letterSpacing: '-0.10em',
-    fontWeight: 'bold',
-}
-
-const descriptionStyle = {
-    fontFamily: 'karla',
-    
-}
-
-const divisorStyle = {
-    marginBottom: '50px',
-    
 }
 
 export default WorkCard
