06d035102c34b2e1e50c8b206e54a531f6b5ae24
pedro_bras <pedro.c.bras@gmail.com>
2019-08-02 17:02:59 +0200

    creates custom alert message and makes it show when no input is inserted

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -9,6 +9,11 @@ const Intro = () => {
     const inpKey = "Name";
     const inpValue = document.getElementById("inpValue");
     const submitBtn = document.getElementById("submit-button");
+    const closeBtn = document.getElementById("close-button")
+
+    closeBtn.onclick = function() {
+        document.getElementById('alert-message').style.display = "none"
+    }
 
     inpValue.addEventListener("keyup", function(event){
         if (event.keyCode === 13){
@@ -18,6 +23,8 @@ const Intro = () => {
 
     })
 
+    
+
     submitBtn.onclick = function () {
         const key = inpKey;
         const value = inpValue.value;
@@ -26,7 +33,7 @@ const Intro = () => {
             localStorage.setItem(key, value);
             window.open('/hello', "_self");
         } else {
-            window.alert("Enter you name bitch!");
+            document.getElementById('alert-message').style.display = "flex"
         }
     }
 
@@ -34,17 +41,23 @@ const Intro = () => {
 
 
     return (
+        <div>
         <div className="body-index">
+            <div className="alert-message" id="alert-message">
+                <div>Please write a valid name!</div>
+                <div className="close-button" id="close-button">Close</div>
+            </div>
             <div className="intro-message">
                 <div className='centerIntro'>
                     <div>Don't be a stranger !</div>
                     <div className='centerQuestion'>What's your name?</div>
                     <div className='name-submit'>
-                        <input type='text' className='inputBox' id="inpValue" autoComplete="off" placeholder=''></input>
+                        <input type='text' className='inputBox' id="inpValue" autoComplete="off" placeholder='' pattern="regexp"></input>
                         <div id="submit-button">GO!</div>
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     )
 
