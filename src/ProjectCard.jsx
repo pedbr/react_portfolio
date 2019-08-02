6695d635cced44bb029ab70d4a50035b7993f5a9
pedro_bras <pedro.c.bras@gmail.com>
2019-08-02 10:58:35 +0200

    removes inline styling from project cards

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -5,18 +5,18 @@ const ProjectCard = (props) => {
     return (
 
         <div key={project.id}>
-            <img src={project.image} className="block h-auto w-full" style={{ height: '150px', objectFit: 'cover' }} />
+            <img src={project.image} className=""  />
 
 
-            <div className="px-6 py-4" style={{ minHeight: '150px' }} >
-                <div style={projectTitle} className="font-bold text-xl mb-2">{project.name} .</div>
-                <p className="text-black text-base">
+            <div className=""  >
+                <div className="">{project.name} .</div>
+                <p className="">
                     {project.description}
                 </p>
             </div>
 
-            <div className="flex items-center justify-between leading-none p-2 md:p-4">
-                <a href={project.website} target="_blank" style={detailsBtn} className="bg-black text-yellow hover:bg-grey-darkest text-xs py-1 px-2">
+            <div className="">
+                <a href={project.website} target="_blank" className="">
                     Details .
                 </a>
             </div>
@@ -25,16 +25,5 @@ const ProjectCard = (props) => {
     )
 }
 
-const detailsBtn = {
-    fontFamily: 'karla',
-    letterSpacing: '-0.10em',
-    textDecoration: 'none'
-}
-
-const projectTitle = {
-    fontFamily: 'karla',
-    letterSpacing: '-0.10em',
-    fontWeight: 'bold',
-}
 
 export default ProjectCard
